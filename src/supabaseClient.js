8d8eada1b452b7274edf460d4f59877c2cd0dc89
ThompyTime <christopherjohnthompson_9@outlook.com>
2025-05-02 17:42:51 +0100

    Fix register flow and Supabase policies

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,10 @@
+// src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+
+console.log('URL:', supabaseUrl)
+console.log('Anon Key:', supabaseAnonKey)
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
